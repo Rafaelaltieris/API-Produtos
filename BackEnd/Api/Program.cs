00dc621a015ef217855b072a4f28b989f18fb576
Rafael Altieris <raphaelaltieris@gmail.com>
2025-04-22 15:26:33 -0300

    Adicionando BackEnd de Produtos ao Git

---
+++ BackEnd/Api/Program.cs
@@ -0,0 +1,29 @@
+using Api.Data;
+using Api.Repositorios.Interfaces;
+using Api.Repositorios;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Banco")));
+
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ BackEnd/Api/Repositorios/ProdutoRepository.cs
@@ -0,0 +1,48 @@
+﻿using Api.Data;
+using Api.Models;
+using Api.Repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Api.Repositorios
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Produto>> GetAllAsync()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+
+        public async Task<Produto?> GetByIdAsync(int id)
+        {
+            return await _context.Produtos.FindAsync(id);
+        }
+
+        public async Task AddAsync(Produto produto)
+        {
+            await _context.Produtos.AddAsync(produto);
+        }
+
+        public void Update(Produto produto)
+        {
+            _context.Produtos.Update(produto);
+        }
+
+        public void Delete(Produto produto)
+        {
+            _context.Produtos.Remove(produto);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+    }
+}
